f5c221d34f9f48bfccac59ae7fe6470b16dac147
bleriot nogguia <nstevebleriot@yahoo.fr>
2021-01-31 14:49:43 +0100

    register user & save data

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,19 +4,39 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/signup", name="register")
      */
-    public function index(): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData();
+            $password = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+
         return $this->render('register/index.html.twig', [
             'form' => $form->createView()
         ]);
